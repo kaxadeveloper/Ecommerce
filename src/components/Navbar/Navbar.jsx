a84cfc0280837563c8017799a4b8c9b5db995ac1
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-11 12:38:51 +0400

    Ecommerce

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <div className="container">
+                    <div>
+                        <a href="#" 
+                        className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
+                            ShopHub
+                        </a>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
