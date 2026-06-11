7d9058f1859c54a88a0c9b873362a13d94d70bc7
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-11 15:56:54 +0400

    add styles to main div

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            <div>
+        <div className='bg-white dark:bg-gray-900 dark:text-white'>
+            <div className='py-4'>
                 <div className="container">
                     <div>
-                        <a href="#" 
-                        className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
+                        <a href="#"
+                            className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
                             ShopHub
                         </a>
                         <div>
