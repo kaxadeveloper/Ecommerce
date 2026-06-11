cba4cd5ab563f36c6a0d64bff001776710fbae1e
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-11 17:17:48 +0400

    add MenuLinks array

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const MenuLinks = [
+    {
+       id: 1,
+       name: "Home",
+       link: "/#", 
+    },
+    {
+       id: 2,
+       name: "Shop",
+       link: "/#shop", 
+    },
+    {
+       id: 3,
+       name: "About",
+       link: "/#about", 
+    },
+    {
+       id: 4,
+       name: "Blogs",
+       link: "/#blog", 
+    },
+]
 
 const Navbar = () => {
     return (
