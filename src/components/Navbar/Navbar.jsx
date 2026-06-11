80393596b93bed8639c7dfda5e2bccd7276fd3b9
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-11 16:57:14 +0400

    add div

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='bg-white dark:bg-gray-900 dark:text-white'>
+        <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
             <div className='py-4'>
                 <div className="container">
                     <div>
@@ -10,6 +10,7 @@ const Navbar = () => {
                             className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
                             ShopHub
                         </a>
+                        <div></div>
                         <div>
                             
                         </div>
