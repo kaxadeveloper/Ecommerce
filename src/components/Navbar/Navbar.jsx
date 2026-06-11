ca4f59f49614260aa37612383af2d5cf7216b707
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-11 18:13:54 +0400

    add IoMdSearch icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { IoMdSearch } from 'react-icons/io';
 
 const MenuLinks = [
     {
-       id: 1,
-       name: "Home",
-       link: "/#", 
+        id: 1,
+        name: "Home",
+        link: "/#",
     },
     {
-       id: 2,
-       name: "Shop",
-       link: "/#shop", 
+        id: 2,
+        name: "Shop",
+        link: "/#shop",
     },
     {
-       id: 3,
-       name: "About",
-       link: "/#about", 
+        id: 3,
+        name: "About",
+        link: "/#about",
     },
     {
-       id: 4,
-       name: "Blogs",
-       link: "/#blog", 
+        id: 4,
+        name: "Blogs",
+        link: "/#blog",
     },
 ]
 
@@ -36,16 +37,27 @@ const Navbar = () => {
                         <div className='hidden lg:block'>
                             <ul className='flex items-center gap-4'>
                                 {
-                                   MenuLinks.map((data, index) => (
-                                     <li key={index}>
-                                        <a href={data.link}
-                                        className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200 '
-                                        >{data.name}</a>
-                                     </li>
-                                   ))
+                                    MenuLinks.map((data, index) => (
+                                        <li key={index}>
+                                            <a href={data.link}
+                                                className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200 '
+                                            >{" "}{data.name}</a>
+                                        </li>
+                                    ))
                                 }
                             </ul>
                         </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <input type="text"
+                                placeholder='Search'
+                                className='' />
+                                <IoMdSearch 
+                                />
+                        </div>
+                        <div></div>
                     </div>
                 </div>
             </div>
