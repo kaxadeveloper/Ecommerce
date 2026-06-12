e5c433cdf40edeec19d4fcbd9677c79648e84648
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-12 22:10:06 +0400

    add two photos

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import LightButton from "../../assets/website/light-mode-button.png";
+import DarkButton from "../../assets/website/dark-mode-button.png";
 
 const DarkMode = () => {
   return (
     <div>
-      
+        
     </div>
   )
 }
