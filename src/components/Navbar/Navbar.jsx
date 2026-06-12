2d17e82150fa225d9a7d63e6f3c7b78e6aae73e7
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-12 21:01:13 +0400

    add styles to button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
                             <IoMdSearch
                                 className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200' />
                         </div>
-                        <button>
+                        <button className='relative p-3'>
                             <FaCartShopping className='text-xl text-gray-600 dark:text-gray-400'/>
                         </button>
                         <div></div>
