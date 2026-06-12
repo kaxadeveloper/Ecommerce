5ed431df576ad2e477ffad662fa8bc9555f5ea59
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-12 20:58:02 +0400

    add styles to icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
                                 className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200' />
                         </div>
                         <button>
-                            <FaCartShopping />
+                            <FaCartShopping className='text-xl text-gray-600 dark:text-gray-400'/>
                         </button>
                         <div></div>
                     </div>
