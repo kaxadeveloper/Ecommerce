7cca7bdd3f85e6b3602c515e6a37a471c2aef831
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-12 20:24:43 +0400

    add styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
     return (
         <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
             <div className='py-4'>
-                <div className="container">
+                <div className="container flex justify-between items-center">
                     <div className='flex items-center gap-4'>
                         <a href="#"
                             className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
