44dd7ae646db7fd1c246f5a5e1bc61c0fc161269
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-13 22:18:22 +0400

    add transition and duration

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -9,7 +9,7 @@ const DarkMode = () => {
         <div className='relative'>
             <img onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={LightButton} alt=""
                 className={`w-7 cursor-pointer absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : "opacity-100"
-                    }`}
+                    } transition-all duration-300`}
             />
             <img onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={DarkButton} alt=""
                 className={`w-7 cursor-pointer`}
