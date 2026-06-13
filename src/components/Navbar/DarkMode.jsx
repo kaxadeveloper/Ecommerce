7c4e59c5036c07b017c7b05a084a8a76dd3005e3
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-13 22:07:43 +0400

    add styles to LightButton

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -3,12 +3,12 @@ import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
 
 const DarkMode = () => {
-    const [theme, setTheme] = React.useState("dark");
+    const [theme, setTheme] = React.useState("light");
 
     return (
         <div className='relative'>
             <img src={LightButton} alt=""
-                className={`w-7 cursor-pointer ${theme === "dark" ? "opacity-0" : "opacity-100"
+                className={`w-7 cursor-pointer absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : "opacity-100"
                 }`}
             />
             <img src={DarkButton} alt=""
