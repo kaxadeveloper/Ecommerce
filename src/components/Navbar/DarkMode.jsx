18c864ec4b252ab7b13e82f59a987b0fff05f778
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-13 21:42:45 +0400

    add styles to LightButton

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -3,11 +3,13 @@ import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
 
 const DarkMode = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <img src={LightButton} alt=""
+                className={`w-12 cursor-pointer`}
+            />
+        </div>
+    )
 }
 
 export default DarkMode
