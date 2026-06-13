08e081fe7d710197704e2680751e9fbdad5bb2ca
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-13 22:00:28 +0400

    add DarkButton

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -3,13 +3,16 @@ import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
 
 const DarkMode = () => {
-    const [theme, setTheme] = React.useState("light");
+    const [theme, setTheme] = React.useState("dark");
 
     return (
         <div>
             <img src={LightButton} alt=""
                 className={`w-7 cursor-pointer ${theme === "dark" ? "opacity-0" : "opacity-100"
                 }`}
+            />
+            <img src={DarkButton} alt=""
+                className={`w-7 cursor-pointer`}
             />
         </div>
     )
