43ad9b697fe3bc0e1df832a5c7d81ac4eb0fcf6b
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-14 14:17:07 +0400

    add styles to a tag

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
                                             {
                                                 DropdownLinks.map((data, index) => (
                                                     <li>
-                                                      <a href={data.link}>{data.name}</a>
+                                                      <a className='text-gray-500 hover:text-black dark:hover:text-wh duration-200' href={data.link}>{data.name}</a>
                                                     </li>
                                                 ))
                                             }
