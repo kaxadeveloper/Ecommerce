7e658b90d70e2574113d256e5103788c969062fd
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-15 07:52:25 +0400

    add divs in slider

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,15 +15,32 @@ const Hero = () => {
         pauseOnFocus: true,
     };
 
-  return (
-    <div>
-      <div>
-        <Slider>
-          
-        </Slider>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h3>4</h3>
+                    </div>
+                    <div>
+                        <h3>5</h3>
+                    </div>
+                    <div>
+                        <h3>6</h3>
+                    </div>
+                </Slider>
+            </div>
+        </div>
+    )
 }
 
 export default Hero
