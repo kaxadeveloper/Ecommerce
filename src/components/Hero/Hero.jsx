5b06bf2e3fe5de8374f3436cdeda6b0fd0e2891d
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-15 08:50:02 +0400

    import 3 image in to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import Slider from "react-slick";
+import Image1 from "../../assets/hero/headphone.jpg";
+import Image2 from "../../assets/category/VR.webp";
+import Image3 from "../../assets/category/macbook.jpg";
 
 const HeroSlide = [
     {
-        
-    }
+        id: 1,
+        img: Image1,
+        subtitle: "Beats Solo",
+        title: "Wireless",
+        title2: "Headphone",
+    },
+    {
+        id: 2,
+        img: Image2,
+        subtitle: "Beats Solo",
+        title: "Wireless",
+        title2: "Virtual",
+    },
+    {
+        id: 3,
+        img: Image3,
+        subtitle: "Beats Solo",
+        title: "Branded",
+        title2: "Laptops",
+    },
 ]
 
 const Hero = () => {
