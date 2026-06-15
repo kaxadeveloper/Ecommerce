dd0ca1249becbf73deb556086ac09364932aa7af
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-15 08:03:47 +0400

    add HeroSlide

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Slider from "react-slick";
 
+const HeroSlide = () => {
+    
+}
+
 const Hero = () => {
 
     const settings = {
@@ -17,26 +21,9 @@ const Hero = () => {
 
     return (
         <div>
-            <div>
+            <div className='w-full'>
                 <Slider {...settings}>
-                    <div>
-                        <h3>1</h3>
-                    </div>
-                    <div>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
-                    </div>
+                    
                 </Slider>
             </div>
         </div>
