70281fbe89a84dd7c836111cee26445ff7edefac
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-17 15:17:51 +0400

    add styles to title's header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -57,7 +57,7 @@ const Hero = () => {
                                     <div className='grid grid-cols-1 sm:grid-cols-2'>
                                         <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
                                             <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
-                                            <h1>{data.title}</h1>
+                                            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
                                             <h1>{data.title2}</h1>
                                             <div>
                                                 <button>Shop Now</button>
