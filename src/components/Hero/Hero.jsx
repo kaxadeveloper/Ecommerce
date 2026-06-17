497f3dc99c7c8ca2d7ca00a720847eae6f410ed9
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-17 16:04:30 +0400

    add more styles to img tag

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -67,7 +67,7 @@ const Hero = () => {
                                         <div className='order-1 sm:order-2'>
                                             <div>
                                                 <img src={data.img} alt=""
-                                                    className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain  mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40'
+                                                    className='w-[300px] sm:w-[450px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-120 object-contain  mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40'
                                                 />
                                             </div>
                                         </div>
