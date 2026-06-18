e005792ed0ed459e4d5bfa231f311a8595788784
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-18 13:37:27 +0400

    add 3 p tags to Category component

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -6,7 +6,19 @@ import Image3 from "../../assets/category/macbook.jpg";
 const Category = () => {
     return (
         <div className='py-8'>
-            <div></div>
+            <div className='container'>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+                    <div>
+                        <div>
+                            <div>
+                                <p>Enjoy</p>
+                                <p>With</p>
+                                <p>Earphone</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
