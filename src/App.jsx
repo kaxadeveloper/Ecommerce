55e53b0d44727a4bcf83b1e9ef739d7e84a5fa82
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-20 21:21:15 +0400

    add Services component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
+import Services from './components/Services/Services'
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <Hero />
       <Category />
       <Category2 />
-      
+      <Services />
     </div>
   )
 }
