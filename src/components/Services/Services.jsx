72186205f3003b350b60aba9cff1c700d776cfbd
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-20 21:38:20 +0400

    import react-icons

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { FaCarSide, FaHeadphonesAlt, FaWallet, FaCheckCircle } from 'react-icons/fa';
 
 const Services = () => {
     return (
         <div>
             <div className="container">
-                
+                <div className="grid">
+                    <div></div>
+                </div>
             </div>
         </div>
     )
