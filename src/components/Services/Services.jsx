efc1fda65d14ab8f3a7a5ce5b69881e0a9c982fe
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-20 21:54:51 +0400

    add ServiceData array

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { FaCarSide, FaHeadphonesAlt, FaWallet, FaCheckCircle } from 'react-icons/fa';
 
+const ServicesData = [
+    {
+        id: 1,
+        icon: <FaCarSide className='text-4xl md:text-5xl text-primary' />,
+        title: "Free Shipping",
+        description: "Free Shipping On All Order",
+    },
+    {
+        id: 2,
+        icon: <FaCheckCircle className='text-4xl md:text-5xl text-primary' />,
+        title: "Safe Money",
+        description: "30 Days Money Back",
+    },
+    {
+        id: 3,
+        icon: <FaWallet className='text-4xl md:text-5xl text-primary' />,
+        title: "Secure Payment",
+        description: "All Payment Secure",
+    },
+    {
+        id: 4,
+        icon: <FaHeadphonesAlt className='text-4xl md:text-5xl text-primary' />,
+        title: "Online Support 24/7",
+        description: "Technical Support 24/7",
+    },
+]
+
 const Services = () => {
     return (
         <div>
