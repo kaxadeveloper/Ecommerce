059afc4d7092664506ca9ba40e82519d3738faa9
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-21 21:17:12 +0400

    BannerData is a data object that contains the banner's text content, image, discount information, sale dates, and background color

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import Hero from './components/Hero/Hero'
 import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
 import Services from './components/Services/Services'
+import Banner from './components/Banner/Banner'
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "25 Dec to 14 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, nisi?",
+  bgColor: "#f42c37",
+};
 
 const App = () => {
   return (
@@ -13,6 +25,7 @@ const App = () => {
       <Category />
       <Category2 />
       <Services />
+      <Banner />
     </div>
   )
 }
