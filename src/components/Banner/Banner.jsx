870c5dfbc7ba834dc23bbe8be826dc2ef1801542
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-22 08:17:53 +0400

    This class makes the text uppercase, bold, 36px by default, and 72px on large screens, making it ideal for large headings or promotional text.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = ({ data }) => {
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl'>
                     <div className='p-6 sm:p-8'>
                         <p className='text-sm'>{data.discount}</p>
-                        <h1> {data.title}</h1>
+                        <h1 className='uppercase text-4xl lg:text-7xl font-bold'>{" "}{data.title}</h1>
                         <p>{data.date}</p>
                     </div>
                 </div>
