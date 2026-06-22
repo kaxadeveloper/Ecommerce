a11d82cc69f699a8d562408899e35f3946c94986
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-22 08:30:32 +0400

    change styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Banner = ({ data }) => {
     return (
         <div className='min-h-[550px] flex justify-center items-center py-12'>
-            <div style={{ backgroundColor: data.bgColor }} className='container'>
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl'>
+            <div className='container'>
+                <div style={{ backgroundColor: data.bgColor }} className='grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl'>
                     <div className='p-6 sm:p-8'>
                         <p className='text-sm'>{data.discount}</p>
                         <h1 className='uppercase text-4xl lg:text-7xl font-bold'>{" "}{data.title}</h1>
