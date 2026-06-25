ace26bae3f39b4bd6e0fd5183e872188a8e09dc6
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-25 18:57:06 +0400

    add styles

--- src/components/Shared/Heading.jsx
+++ src/components/Shared/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading = ({ title, subtitle }) => {
     return (
-        <div>
+        <div className='text-center mb-10 max-w-[600px] mx-auto space-y-2'>
            <h1>{title}</h1>
            <p>{subtitle}</p>
         </div>
