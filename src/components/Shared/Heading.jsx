13011539a2678e7db3df47dd5cddc14006c5da5e
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-25 18:59:16 +0400

    add styles to title

--- src/components/Shared/Heading.jsx
+++ src/components/Shared/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = ({ title, subtitle }) => {
     return (
         <div className='text-center mb-10 max-w-[600px] mx-auto space-y-2'>
-           <h1>{title}</h1>
+           <h1 className='text-3xl font-bold lg:text-4xl'>{title}</h1>
            <p>{subtitle}</p>
         </div>
     )
