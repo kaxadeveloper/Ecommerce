b6384ff0dc4a774da7f4e14e1b64edab21309087
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-25 18:51:55 +0400

    add props to Heading component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Heading from '../Shared/Heading'
 
 const Products = () => {
     return (
         <div>
             <div className="container">
-                
+                <Heading title="Our Products" subtitle={"Explore Our Products"}/>
             </div>
         </div>
     )
