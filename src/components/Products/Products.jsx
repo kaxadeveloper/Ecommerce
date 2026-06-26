420ffb5c9f09d6b0202513af9695b1a9d259bdee
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-26 14:06:06 +0400

    create ProductsData array

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,37 @@
 import React from 'react'
 import Heading from '../Shared/Heading'
 
+const ProductsData = [
+    {
+        id: 1,
+        img: Img1,
+        title: "Boat Headphone",
+        price: "120",
+        aosDelay: "0",
+    },
+    {
+        id: 2,
+        img: Img2,
+        title: "Rocky Mountain",
+        price: "420",
+        aosDelay: "200",
+    },
+    {
+        id: 3,
+        img: Img3,
+        title: "Goggles",
+        price: "320",
+        aosDelay: "400",
+    },
+    {
+        id: 4,
+        img: Img4,
+        title: "Printed",
+        price: "220",
+        aosDelay: "600",
+    },
+]
+
 const Products = () => {
     return (
         <div>
