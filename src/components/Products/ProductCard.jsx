d367df03eb9425f35feafa63b207558b0d4e16e3
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-26 15:12:14 +0400

    add key

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ProductCard = ({ data }) => {
     return (
-        <div>
-            <div>
+        <div className='mb-10'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 place-items-center'>
                 {
                     data.map((data) => (
-                        <div>
+                        <div key={data.id}>
                             <div>
                                 <img src={data.img} alt=""
                                     className='[h-180px] w-[260px] object-cover rounded-md'
