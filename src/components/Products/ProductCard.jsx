d07819d4d1578b3c76fd2ffa6e2bf7b74241c57f
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-26 16:05:07 +0400

    add styles

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({ data }) => {
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 place-items-center'>
                 {
                     data.map((data) => (
-                        <div key={data.id}>
+                        <div className='group' key={data.id}>
                             <div className='relative'>
                                 <img src={data.img} alt=""
                                     className='h-[180px] w-[260px] object-contain rounded-md'
