984af972409355aa082312db90f040a8f0d93a39
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-26 14:51:48 +0400

    add styles to img

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const ProductCard = ({ data }) => {
     return (
         <div>
-
+            <div>
+                {
+                    data.map((data) => (
+                        <div>
+                            <div>
+                                <img src={data.img} alt=""
+                                    className='[h-180px] w-[260px] object-cover rounded-md'
+                                />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import Heading from '../Shared/Heading'
+import ProductCard from './ProductCard'
+
+import Img1 from "../../assets/product/p-1.jpg";
+import Img2 from "../../assets/product/p-2.jpg";
+import Img3 from "../../assets/product/p-3.jpg";
+import Img4 from "../../assets/product/p-4.jpg";
+import Img5 from "../../assets/product/p-5.jpg";
+import Img6 from "../../assets/product/p-6.jpg";
+import Img7 from "../../assets/product/p-7.jpg";
+import Img8 from "../../assets/product/p-8.jpg";
 
 const ProductsData = [
     {
