269f75ebb3202d8a598607fd7e1101f45f7f7d61
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-27 17:16:52 +0400

    add styles

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,7 +5,10 @@ const Blogs = () => {
     return (
         <div>
             <div className="container">
-                <Heading title="Recent News" subtitle={"Explore Our Blogs"}/>
+                <Heading title="Recent News" subtitle={"Explore Our Blogs"} />
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 gap-y-8 sm:gap-4 md:gap-7'>
+
+                </div>
             </div>
         </div>
     )
