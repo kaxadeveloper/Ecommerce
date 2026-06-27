937a09c25ee95b4d19760d69e8a5f6e30e50db60
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-27 16:34:18 +0400

    add Heading component

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Heading from '../Shared/Heading'
 
 const Blogs = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className="container">
+                <Heading />
+            </div>
+        </div>
+    )
 }
 
 export default Blogs
