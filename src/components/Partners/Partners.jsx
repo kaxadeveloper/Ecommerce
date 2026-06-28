61435aee6999ef8a76ada2d80c6a1b57415cbbab
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-28 10:13:03 +0400

    add img

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Partners = () => {
-  return (
-    <div>
-      <div className="container">
-        <div className="grid">
-            
+    return (
+        <div>
+            <div className="container">
+                <div className="grid">
+                    <img src={brand1} alt="brand" className='w-[80px] dark:invert' />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Partners
