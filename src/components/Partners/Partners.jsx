c0d3a7d854a89817b4a403ff65a805cfacefc9bc
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-28 10:15:05 +0400

    add more img tags

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -6,6 +6,10 @@ const Partners = () => {
             <div className="container">
                 <div className="grid">
                     <img src={brand1} alt="brand" className='w-[80px] dark:invert' />
+                    <img src={brand2} alt="brand" className='w-[80px] dark:invert' />
+                    <img src={brand3} alt="brand" className='w-[80px] dark:invert' />
+                    <img src={brand4} alt="brand" className='w-[80px] dark:invert' />
+                    <img src={brand5} alt="brand" className='w-[80px] dark:invert' />
                 </div>
             </div>
         </div>
