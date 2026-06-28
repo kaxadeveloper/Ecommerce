36c7f09f33635fd32350370fe97b5dc85e2a3e4d
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-28 11:07:29 +0400

    add styles

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -9,7 +9,7 @@ const Partners = () => {
     return (
         <div className='py-8 mt-24 hidden md:block bg-gray-200 dark:bg-white/10'>
             <div className="container">
-                <div className="grid">
+                <div className="grid grid-cols-5 gap-3 place-items-center opacity-50">
                     <img src={brand1} alt="brand" className='w-[80px] dark:invert' />
                     <img src={brand2} alt="brand" className='w-[80px] dark:invert' />
                     <img src={brand3} alt="brand" className='w-[80px] dark:invert' />
