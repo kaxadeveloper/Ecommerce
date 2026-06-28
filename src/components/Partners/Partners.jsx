d00ba632a46b39bfdb011928228406809a770e37
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-28 10:05:26 +0400

    add divs to Partner component

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Partners = () => {
   return (
     <div>
-      
+      <div className="container">
+        <div className="grid">
+            
+        </div>
+      </div>
     </div>
   )
 }
