0416b1726e3e486352064903e0d9a2f033839402
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-29 19:27:50 +0400

    add a tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-      <div className="container">
-        
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="container">
+                <div className="grid">
+                    <div>
+                        <a
+                            href="#"
+                            className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>
+                                Shophub
+                            </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
