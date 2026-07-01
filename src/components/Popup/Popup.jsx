40cfd945913b61c36b98fcf76ec6d9cad8467ae3
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-07-01 11:16:24 +0400

    add styles

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Popup = ({ orderPopup, setOrderPopup }) => {
     return (
         <div>
-
+            <div className='h-screen w-screen fixed top-0'>
+                <div>
+                    <div></div>
+                </div>
+            </div>
         </div>
     )
 }
