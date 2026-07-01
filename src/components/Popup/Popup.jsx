4ad65bb92efac88ec73c6ae5409d7447b6c2af38
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-07-01 11:42:50 +0400

    add header

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -4,8 +4,10 @@ const Popup = ({ orderPopup, setOrderPopup }) => {
     return (
         <div>
             <div className='h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm'>
-                <div className='w-[300px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
-                    <div></div>
+                <div className='w-[300px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 rounded-md'>
+                    <div>
+                        <h1>Order Now</h1>
+                    </div>
                 </div>
             </div>
         </div>
